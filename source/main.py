b78a612e9a39745d79e77ff36a1ecfeebb945337
FacundoMajda <facundomajda13@gmail.com>
2023-06-15 14:51:17 -0300

    creacion de modulos

--- source/main.py
+++ source/main.py
@@ -1,59 +1,59 @@
 #-----------------------------------------------------
 # ASISTENTE VIRTUAL POR VOZ
-# Facundo Majda - IPF
+# IPF
 # 2023 - FORMOSA, ARGENTINA
-#stormbreaker
 #-----------------------------------------------------
-
-
-
-
-#module text to speech here.
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("Hola usuario, soy un modelo de reconocimiento de voz a tiempo real, por favor habla e intentaré responder")
-engine.runAndWait()
-
 
 #use google speech recognition to this project
 #most accesible tool of this type. Free public api 
- 
+
+
+
+
+import pyttsx3
+import time
 import speech_recognition as sr
 
-r=sr.speech()
-
-with sr.Microphone() as source:
-    print("Hola, soy un modelo de reconocimiento de voz a tiempo real, por favor habla e intentaré responder")
-    audio = r.listen(source)
-    try: 
-        text = r.recognize_google(audio, language="es") 
-        print(text) 
-    except: 
-     print("Usuario, no te he entendido, por favor vuelve a hablar")
+engine = pyttsx3.init()
+r = sr.Recognizer()
 
 
-
-
-
-
-
+#Speech Module
 class SpeechModule:
-   def __init__(self, voice= 0, volume=1, rate=125):
+    def __init__(self, voice=0, volume=1, rate=120):
         self.engine = pyttsx3.init()
         self.engine.setProperty('rate', rate)
         self.engine.setProperty('volume', volume)
-                
         voices = self.engine.getProperty('voices')
         self.engine.setProperty('voice', voices[voice].id)
         
-        
-def talk(self, text):
+    def talk(self, text):
         self.engine.say(text)
         self.engine.runAndWait()
 
 
-
+#Voice recognition module
 class VoiceRecognitionModule:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, key=None):
+        #aqui va la key de la API de google speech recognition
+        self.key = key
+        self.r = sr.Recognizer()
+    
+    def recognize(self):
+        with sr.Microphone() as source:
+            print("Di algo ahora! :")
+        audio = self.r.listen(source)
+        try: 
+            text = self.r.recognize_google(audio,key=self, language="es") 
+            return text 
+        except: 
+              return None
 
+speech= SpeechModule()
+text= VoiceRecognitionModule()
+
+while True:
+    text = recognition.recognize()
+    if text:
+        speech.talk(text)
+    time.sleep(1)
